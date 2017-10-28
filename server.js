76b494f7c65129290e81a2f3b33aff27d3d178cb
Aioub <ayyub.amini@gmail.com>
2017-10-29 00:02:43 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,17 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
 
-// set up handlebars view engine
-var handlebars = require('express3-handlebars')
-  .create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+var port = process.env.PORT || 8080;
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
-})
+// set the view engine to ejs
+app.set('view engine', 'html');
 
-// Home Page
-app.get('/', function(req, res){
-  res.render('home');
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
-// About Page
-app.get('/about', function(req, res){
-  res.render('about');
-});
-
-// 404 catch-all handler (middleware)
-app.use(function(req, res){
-  res.status(404);
-  res.render('404');
-});
-
-// 500 error handler (middleware)
-app.use(function(req, res){
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
